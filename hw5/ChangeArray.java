ea2ddd51bac4c31d625cf334420af98147b0e9e4
yanaPy <110561047+yanaPy@users.noreply.github.com>
2023-10-07 20:50:54 +0300

    update hw5 with comment resolve

--- hw5/ChangeArray.java
+++ hw5/ChangeArray.java
@@ -4,14 +4,13 @@ import java.util.Arrays;
 
 public class ChangeArray {
 
-  /*  3. Написати програму, яка в масиві з випадкових цілих чисел замінює всі парні числа на 0 і
-    виводить отриманий масив на екран.*/
+    /*  3. Написати програму, яка в масиві з випадкових цілих чисел замінює всі парні числа на 0 і
+      виводить отриманий масив на екран.*/
     static void changeArray(int[] array) {
 
         int[] copyArray = new int[array.length];
 
-        for (int i = 0; i < array.length - 1; i++) {
-
+        for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
                 copyArray[i] = 0;
             } else {
