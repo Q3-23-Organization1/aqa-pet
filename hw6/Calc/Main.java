8f8641de08d3d39a4e9c48e9f8612c1a9f273ba2
yanaPy <110561047+yanaPy@users.noreply.github.com>
2023-10-10 21:21:27 +0300

    update hw6 comment resolve

---
+++ hw6/Calc/Main.java
@@ -0,0 +1,14 @@
+package hw6.Calc;
+
+import static hw6.Calc.Calculator.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(addition(10,1));
+        System.out.println(subtraction(10,1));
+        System.out.println(division(10,2));
+        System.out.println(multiplication(10,2));
+    }
+}
